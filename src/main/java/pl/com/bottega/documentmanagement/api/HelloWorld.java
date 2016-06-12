16de860e8ceb8fff0a267972d9fc8a22c65ae82d
BernardBoguszewski <bernardboguszewski@gmail.com>
2016-06-12 11:34:56 +0200

    Added main mathod

--- src/main/java/pl/com/bottega/documentmanagement/api/HelloWorld.java
+++ src/main/java/pl/com/bottega/documentmanagement/api/HelloWorld.java
@@ -4,4 +4,7 @@ package pl.com.bottega.documentmanagement.api;
  * Created by bernard.boguszewski on 12.06.2016.
  */
 public class HelloWorld {
+  public static void main(String[] args){
+    
+  }
 }
